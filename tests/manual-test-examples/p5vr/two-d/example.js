077cb4181d05ce547b1156c0ee972ca7cebeb914
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-05-07 19:18:51 -0700

    add simple 2D example

---
+++ tests/manual-test-examples/p5vr/two-d/example.js
@@ -0,0 +1,41 @@
+let pg;
+let pos, vel;
+let speed = 1.5, ellipseSize = 20;
+
+function preload() {
+  createVRCanvas();
+}
+
+function setup() {
+  setVRBackgroundColor(200, 0, 150);
+  pg = createGraphics(300, 150);
+  vel = createVector(speed, speed);
+  pos = createVector(width/2, height/2);
+  textureMode(NORMAL);
+}
+
+function draw() {
+  pg.background(150, 150, 250);
+  pg.ellipse(pos.x, pos.y, 20);
+
+  pos.x += vel.x;
+  pos.y += vel.y;
+
+  testBounds();
+
+  translate(0, 0, 10);
+  noStroke();
+  texture(pg);
+  rect(-10, -20, 20, 40);
+  
+}
+
+function testBounds() {
+  let rad = ellipseSize/2;
+  if(pos.x > pg.width - rad || pos.x < 0 + rad) {
+    vel.x *= -1;
+  }
+  if(pos.y > pg.height - rad || pos.y < 0 + rad) {
+    vel.y *= -1;
+  }
+}
