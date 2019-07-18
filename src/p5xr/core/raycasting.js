f4b78deabab5b65533343cedb8d6dc1bf229c979
vedhant <vedhantagarwal2@gmail.com>
2019-07-18 22:14:57 +0530

    Implement generateRay()

--- src/p5xr/core/raycasting.js
+++ src/p5xr/core/raycasting.js
@@ -27,8 +27,17 @@ function getRayFromScreen(screenX, screenY) {
 
 // TODO: check what happens when called twice (one for each eye) and screenX and screenY are offset
 
-p5.prototype.intersectsSphere = function(radius, screenX, screenY) {
-  let ray = getRayFromScreen(screenX, screenY);
+p5.prototype.intersectsSphere = function(radius, arg2, arg3) {
+  let ray;
+  if(arg3) {
+    ray = getRayFromScreen(arg2, arg3);
+  }
+  else {
+    ray = arg2;
+  }
+
+  if(ray === null)
+    return false;
     
   // sphere in View space
   let uMVMatrix = p5.instance._renderer.uMVMatrix.copy();
@@ -41,10 +50,6 @@ p5.prototype.intersectsSphere = function(radius, screenX, screenY) {
   sphereCenter.z = uMVMatrix[11];
   // TODO: scaling sphere radius
 
-  // ray sphere intersection algorithm
-  // let t = p5.Vector.dot(p5.Vector.sub(sphereCenter, ray.origin), ray.direction);
-  // let p = p5.Vector.add(ray.origin, );
-
   let sphereToRayOrigin = p5.Vector.sub(ray.origin, sphereCenter);
   let b = 2 * p5.Vector.dot(ray.direction, sphereToRayOrigin);
   let c = p5.Vector.mag(sphereToRayOrigin) * p5.Vector.mag(sphereToRayOrigin) - radius * radius;
@@ -52,4 +57,38 @@ p5.prototype.intersectsSphere = function(radius, screenX, screenY) {
   let det = b * b - 4 * c;
 
   return det >= 0;
+};
+
+p5.prototype.generateRay = function(x1, y1, z1, x2, y2, z2) {
+  let origin = new p5.Vector(x1, y1, z1);
+  let direction = new p5.Vector(x2, y2, z2);
+  direction = p5.Vector.sub(direction, origin);
+  direction.normalize();
+    
+  let uMVMatrix = p5.instance._renderer.uMVMatrix.copy();
+  uMVMatrix.transpose(uMVMatrix);
+  uMVMatrix = uMVMatrix.mat4;
+
+  let originCopy = origin.copy();
+  origin.x = uMVMatrix[0] * originCopy.x + uMVMatrix[1] * originCopy.y + uMVMatrix[2] * originCopy.z + uMVMatrix[3];
+  origin.y = uMVMatrix[4] * originCopy.x + uMVMatrix[5] * originCopy.y + uMVMatrix[6] * originCopy.z + uMVMatrix[7];
+  origin.z = uMVMatrix[8] * originCopy.x + uMVMatrix[9] * originCopy.y + uMVMatrix[10] * originCopy.z + uMVMatrix[11];
+    
+  let uNMatrix = p5.instance._renderer.uMVMatrix.copy();
+  uNMatrix.transpose(uNMatrix);
+  uNMatrix.invert(uNMatrix);
+  uNMatrix.transpose(uNMatrix);
+  uNMatrix = uNMatrix.mat4;
+
+  let directionCopy = direction.copy();
+  direction.x = uNMatrix[0] * directionCopy.x + uNMatrix[1] * directionCopy.y + uNMatrix[2] * directionCopy.z + uNMatrix[3];
+  direction.y = uNMatrix[4] * directionCopy.x + uNMatrix[5] * directionCopy.y + uNMatrix[6] * directionCopy.z + uNMatrix[7];
+  direction.z = uNMatrix[8] * directionCopy.x + uNMatrix[9] * directionCopy.y + uNMatrix[10] * directionCopy.z + uNMatrix[11];
+
+  direction.normalize();
+
+  return {
+    origin: origin,
+    direction: direction
+  };
 };
--- tests/manual-test-examples/p5vr/raycasting/generateRay/sketch.js
+++ tests/manual-test-examples/p5vr/raycasting/generateRay/sketch.js
@@ -2,7 +2,7 @@ let vrGlobals = {
   counter: 0
 };
 
-let randomx=[], randomy=[];
+let randomx = [], randomy = [];
 
 function preload() {
   createVRCanvas();
@@ -10,7 +10,7 @@ function preload() {
 
 function setup() {
   setVRBackgroundColor(200, 0, 150);
-  for(let i=0; i<5; ++i) {
+  for (let i = 0; i < 5; ++i) {
     randomx[i] = random(-500, 500);
     randomy[i] = random(-500, 500);
   }
@@ -18,11 +18,19 @@ function setup() {
 
 function draw() {
   setViewerPosition(0, 0, 400);
-  for(let i=0; i<5; ++i) {
+  push();
+  translate(randomx[0], randomy[0]);
+  fill('green');
+  sphere(30);
+  rotateZ(millis() / 1000);
+  line(0, 0, 0, 1000, 1000, 0);
+  let ray = generateRay(0, 0, 0, 1000, 1000, 0);
+  pop();
+  for (let i = 1; i < 5; ++i) {
     push();
     translate(randomx[i], randomy[i]);
     fill('red');
-    if(intersectsSphere(70, 0, 0)) {
+    if (intersectsSphere(70, ray)) {
       fill('blue');
     }
     sphere(70);
