d5358254dd9d4aadfc2d7d6f075f7c174e82362a
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-09-10 14:21:52 +0300

    testing modification for github actions (continuous integration)

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
@@ -81,7 +81,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
