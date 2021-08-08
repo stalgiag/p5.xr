148535ecce21a6d8c4d783c9e0fb498e218987e2
anagondesign <anais5324@gmail.com>
2021-08-07 21:32:05 -0400

    update basic example
    - Added print message to ensure controller detection/tracking
    - Added more comments explaining functions
    - Lowered ground of sketch
    - unable to make a separate function for the ray section, wasn't working so i left it the same

--- examples/visual-art-making-tools/basic/example.js
+++ examples/visual-art-making-tools/basic/example.js
@@ -1,7 +1,7 @@
-//BASIC EXAMPLE 
+// BASIC EXAMPLE 
 // three colors red, yellow, blue
-//floor image of wood
-//access colors using r g b variables
+// floor image of wood
+// access colors using r g b variables
 
 const brushStrokes = [];
 const maxOrbs = 500;
@@ -23,7 +23,7 @@ let uiSize = 0.15;
 
 function preload() {
   createVRCanvas();
-img = loadImage('woodtex.jpg')
+  img = loadImage('woodtex.jpg')
 }
 
 function setup() {
@@ -45,28 +45,30 @@ function draw() {
     hand = left;
   } else {
     // no controller
+    print('In order to draw, please ensure that a controller is connected and tracking')
   }
 
   if (hand) {
     if (hand.trigger && hand.trigger.pressed) {
+      //if triggered is pressed, draw brushStroke at hand's position
       attemptNewBrushStroke(hand);
     }
 
-    // Apply the pose matrix for the hand
-    // Create a ray, note that rays are made with the current
-    // model view matrix applied so it is easier to assign
-    // position and directionality because it is relative to the
-    // current transform, just like rendering
-    push();
-    applyMatrix(hand.pose);
-    ray = generateRay(0, 0, 0, 0, 0, -1);
-    stroke(255);
-    strokeWeight(5);
-    line(0, 0, 0, 0, 0, -100);
-    noStroke();
-    fill(r, g, b);
-    box(0.05);
-    pop();
+  // Apply the pose matrix for the hand
+  // Create a ray, note that rays are made with the current
+  // model view matrix applied so it is easier to assign
+  // position and directionality because it is relative to the
+  // current transform, just like rendering
+  push();
+  applyMatrix(hand.pose);
+  ray = generateRay(0, 0, 0, 0, 0, -1);
+  stroke(255);
+  strokeWeight(5);
+  line(0, 0, 0, 0, 0, -100);
+  noStroke();
+  fill(r, g, b);
+  box(0.05);
+  pop();    
   }
 
   noStroke();
@@ -75,16 +77,20 @@ function draw() {
 }
 
 function drawColorControls() {
+  // generateRay takes an origin location (x, y, z) and a target location (x, y, z) 
+  // and returns an Object with an origin and a direction 
   if (!ray) {
     return;
   }
+
+  // when the hand's ray instersects with red sphere
   // change brush color to red
   push();
   translate(uiRight, uiTop, uiForward);
   if (intersectsSphere(uiSize, ray)) {
-    r=255;
-    g= 60;
-    b= 110;    
+    r = 255;
+    g = 60;
+    b = 110;    
     fill(225, 30, 80);
   } else {
     fill(255, 60, 110);
@@ -92,13 +98,14 @@ function drawColorControls() {
   sphere(uiSize);
   pop();
 
+  // when the hand's ray instersects with yellow sphere
   // change brush color to yellow
   push();
   translate(uiRight, uiMiddle, uiForward);
   if (intersectsSphere(uiSize, ray)) {
-    r=255;
-    g= 210;
-    b= 100;  
+    r = 255;
+    g = 210;
+    b = 100;  
     fill(225, 180, 70);
   } else {
     fill(255, 210, 100);
@@ -106,6 +113,7 @@ function drawColorControls() {
   sphere(uiSize);
   pop();
 
+  // when the hand's ray instersects with blue sphere
   // change brush color to blue
   push();
   translate(uiRight, uiBottom, uiForward);
@@ -127,6 +135,9 @@ function attemptNewBrushStroke(hand) {
     brushStrokes.shift();
   }
   // Each stroke must be an object that stores both the color and the position
+  // so whenever you press the trigger
+  // this adds locations to the brushStrokes drawing array
+  // and the strokes draw at each of those locations every frame
   const brushStroke =
   {
     position: new p5.Vector(hand.position.x, hand.position.y, hand.position.z),
@@ -136,6 +147,12 @@ function attemptNewBrushStroke(hand) {
 }
 
 function drawBrushStrokes() {
+// in order to access the values you stored in the brushStrokes array
+// they must be called here 
+// in this case, the brush is translated to the brushStroke's position
+// which is the hand's x,y,z position within the virtual space
+// and the brushStroke's RGB color values are placed within fill()
+// if you don't do this, your brush won't look the way you want it to
   for (const stroke of brushStrokes) {
     push();
     translate(stroke.position);
@@ -146,13 +163,15 @@ function drawBrushStrokes() {
 }
 
 function drawGround() {
+ // this draws the floor of the sketch
+ // can be any color or texture you desire
   push();
-  translate(0, -1, 0);
+  translate(0, -3, 0);
   rotateX(-90);
   noStroke();
   fill(30, 90, 50);
   texture(img);
   plane(10, 10);
   pop();
-
 }
+
