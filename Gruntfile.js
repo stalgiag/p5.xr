37814834e243ec4abce506205941882348b5fd8b
Stalgia Grigg <stalgia.grigg@gmail.com>
2022-08-10 18:57:59 -0400

    Working jsdoc build

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,17 +24,31 @@ module.exports = (grunt) => {
         },
       },
     },
+    jsdoc2md: {
+      rayCasting:
+        { src: 'src/p5xr/core/raycasting.js', dest: 'docs/reference/raycasting.md' },
+      p5xrInput:
+        { src: 'src/p5xr/core/p5xrInput.js', dest: 'docs/reference/p5xrInput.md' },
+      p5xrViewer:
+        { src: 'src/p5xr/core/p5xrViewer.js', dest: 'docs/reference/p5xrViewer.md' },
+      app:
+            { src: 'src/app.js', dest: 'docs/reference/app.md' },
+    },
   });
 
   // Load task plugins
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-run');
+  grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
 
   // Tasks
   grunt.registerTask('build', [
     'eslint',
     'karma:unit',
     'run:build',
+  ]);
+  grunt.registerTask('docs', [
+    'jsdoc2md',
   ]);
 };
